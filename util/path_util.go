72174d74a56353507220d259ad1feecf09c2838f
bullet <createbybullet@163.com>
2023-05-26 00:10:43 +0800

    用os.TempDir()替换getTmpDir()；删除isTest()

--- util/path_util.go
+++ util/path_util.go
@@ -29,31 +29,13 @@ func GetFilePath(relativePath string) string {
 // GetCurrentAbPath 获取项目的跟路径
 func GetCurrentAbPath() string {
 	dir := getCurrentAbPathByExecutable()
-	if strings.Index(dir, getTmpDir()) == 0 {
+	if strings.Index(dir, os.TempDir()) == 0 {
 		return getCurrentAbPathByCaller()
 	}
 	if dir[len(dir)-1] != '/' {
 		dir += "/"
 	}
 	return dir[:strings.LastIndex(dir, fmt.Sprintf("/%s/", modName))+len(modName)+1]
-}
-
-// getTmpDir 获取系统临时目录，兼容go run
-func getTmpDir() string {
-	switch runtime.GOOS {
-	case "linux":
-		return "/tmp"
-	case "windows":
-		dir := os.Getenv("TEMP")
-		if dir == "" {
-			dir = os.Getenv("TMP")
-		}
-		res, _ := filepath.EvalSymlinks(dir)
-		return res
-	case "darwin":
-		return "/tmp"
-	}
-	return ""
 }
 
 // getCurrentAbPathByExecutable 获取当前执行文件绝对路径
@@ -74,15 +56,4 @@ func getCurrentAbPathByCaller() string {
 		abPath = path.Dir(filename)
 	}
 	return path.Dir(abPath)
-}
-
-// isTest 是否是在单元测试模式下运行
-func isTest() bool {
-	for _, v := range os.Args {
-		v = strings.ToLower(v)
-		if strings.Contains(v, "-test") {
-			return true
-		}
-	}
-	return false
 }
